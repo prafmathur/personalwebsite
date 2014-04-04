3d2947fcc7ff818ab7dd93c05ea36fea43329f33
prafmathur <prafmathur@gmail.com>
2014-04-04 00:53:54 -0400

    initial web commit

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
 
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.render('index.html');
 });
 
 
-app.listen(3000);
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
